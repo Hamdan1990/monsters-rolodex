77df677b8845fccc52e0e5ef2384f01f196afaeb
Hamdan Siddiqui <hamdan@Hamdans-MacBook-Pro.local>
2019-07-08 22:27:33 -0500

    Updates

---
+++ precache-manifest.0b7cc41e8ee42200e58609139357ea3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ac6e10c63679e7693270641c7daa51",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aad85ef1684ebca5bc66",
+    "url": "/monsters-rolodex/static/css/main.9fcab3d0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "aad85ef1684ebca5bc66",
+    "url": "/monsters-rolodex/static/js/main.f144144c.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
